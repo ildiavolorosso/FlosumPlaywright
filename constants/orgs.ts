a8e5ee6a3c4b8ec43b4be9395d1841fa846a8201
JWM <morse.joe@gmail.com>
2025-08-07 14:36:50 -0700

    Added basic Salesforce Sales Cloud test.

---
+++ constants/orgs.ts
@@ -0,0 +1,5 @@
+export const org = {
+    url : 'https://cirruscrm--uat.sandbox.my.salesforce.com/',
+    username : 'admin@cirruscrm.demo.uat',
+    password : 'BrrYbSmw8#DYCg',
+};
---
+++ tests/CirrusSFDC.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+import { org } from '../constants/orgs';
+import { constants } from 'buffer';
+
+test('test', async ({ page }) => {
+  // open the login page
+  await page.goto(org.url);
+  await expect(page.getByRole('textbox', { name: 'Username' })).toBeVisible();
+  await expect(page.getByRole('textbox', { name: 'Password' })).toBeVisible();
+  
+  // log in to the org and test for lightning experience 
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(org.username);
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill(org.password);
+  await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
+  await expect(page.getByRole('button', { name: 'App Launcher' })).toBeVisible({ timeout: 15_000 });
+
+  // navigate to the Sales lightning app
+  await page.getByRole('button', { name: 'App Launcher' }).click({ timeout: 10_000 });
+  await page.getByRole('combobox', { name: 'Search apps and items...' }).click();
+  await page.getByRole('combobox', { name: 'Search apps and items...' }).fill('Sales');
+  await page.getByRole('option', { name: 'Sales', exact: true }).click();
+
+  await expect(page.getByRole('link', { name: 'Opportunities' })).toBeVisible({ timeout: 15_000 });
+  await expect(page.getByRole('link', { name: 'Accounts' })).toBeVisible({ timeout: 15_000 });
+});
